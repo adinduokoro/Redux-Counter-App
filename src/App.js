42812f346981abd3c4d46c7af3bc8eb374bad1fc
Addie Okoro <adinduokoro95@gmail.com>
2023-05-11 23:11:44 -0400

    Store
    Store functionality is mostly created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "../src/App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <div className="counter__wrapper">
+        <div className="counter__number">
+          <span>{count}</span>
+        </div>
+        <div className="counter__btn">
+          <div className="increase__btn">
+            <button onClick={increment}>Increase</button>
+          </div>
+          <div className="decrease__btn">
+            <button onClick={decrement}>Decrease</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state, action) => {
+      state.count += 1;
+    },
+    decrement: (state, action) => {
+      state.count -= 1;
+    },
+  },
+});
+
+export default counterSlice.reducer;
+export const { increment, decrement } = counterSlice.actions;
